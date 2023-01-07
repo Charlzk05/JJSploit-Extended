1065a0bacda2793938205bc8ec0fa9238a984143
charlzk05 <charliezkiecharlzk@gmail.com>
2023-01-07 19:47:23 +0800

    changes 7:46 PM jan 6 2023

--- JJSploit UI Electron/src/page/main.js
+++ JJSploit UI Electron/src/page/main.js
@@ -3,6 +3,9 @@ const buttonsPage = document.getElementsByClassName("buttonsPage")[0];
 const scriptsLibPage = document.getElementsByClassName("scriptsLibPage")[0];
 const navigatorBar = document.getElementsByClassName("navigatorBar")[0];
 const gotoTop = document.getElementsByClassName("gotoTop")[0];
+const scriptName = document.getElementById("scriptName");
+const scriptDesc = document.getElementById("scriptDesc");
+const scriptContent = document.getElementById("scriptContent");
 
 var editor;
 
@@ -48,44 +51,141 @@ scriptsLib_click = () => {
 }
 
 addScript_click = () => {
-    var scriptName = document.getElementById("scriptName");
-    var scriptDesc = document.getElementById("scriptDesc");
-    var scriptContent = document.getElementById("scriptContent");
-    var scriptsLibPage = document.getElementsByClassName("scriptsLibPage")[0];
-
-    Array.from(scriptsLibPage.getElementsByClassName("column")).forEach((column) => {
-        if (column.getElementsByClassName("script").length >= 3) { } else {
-            var scriptContainer = document.createElement("div");
-            scriptContainer.setAttribute("class", "script");
-            var scriptH3 = document.createElement("h3");
-            scriptH3.innerText = scriptName.value;
-            var scriptP = document.createElement("p");
-            scriptP.innerText = scriptDesc.value;
-            var scriptButtons = document.createElement("div");
-            scriptButtons.setAttribute("class", "buttons");
-            var executeButton = document.createElement("button");
-            executeButton.innerText = "Execute";
-            var deleteButton = document.createElement("button");
-            deleteButton.innerText = "Delete";
-            scriptButtons.appendChild(executeButton);
-            scriptButtons.appendChild(deleteButton);
-            var scriptTextArea = document.createElement("textarea");
-            scriptTextArea.setAttribute("style", "display: none;");
-            scriptTextArea.value = scriptContent.value;
-
-            scriptContainer.appendChild(scriptH3);
-            scriptContainer.appendChild(scriptP);
-            scriptContainer.appendChild(scriptButtons);
-            scriptContainer.appendChild(scriptTextArea);
-
-            column.appendChild(scriptContainer);
-        }
-    });
+    if (scriptDesc.value == "") {
+        var scriptContainer = document.createElement("div");
+        scriptContainer.setAttribute("class", "script");
+    
+        var scriptH3 = document.createElement("h3");
+        scriptH3.innerText = scriptName.value;
+    
+        var scriptP = document.createElement("p");
+        scriptP.innerText = "(No description provided)";
+    
+        var scriptButtons = document.createElement("div");
+        scriptButtons.setAttribute("class", "buttons");
+    
+        var scriptContentContainer = document.createElement("div");
+        scriptContentContainer.setAttribute("class", "content");
+        scriptContentContainer.setAttribute("style", "display: none;");
+    
+        var scriptContentP = document.createElement("p");
+        scriptContentP.innerText = "Script";
+    
+        var scriptContentTextArea = document.createElement("textarea");
+        scriptContentTextArea.value = scriptContent.value;
+    
+        scriptContentContainer.appendChild(scriptContentP);
+        scriptContentContainer.appendChild(scriptContentTextArea);
+    
+        var executeButton = document.createElement("button");
+        executeButton.innerText = "Execute";
+    
+        executeButton.addEventListener("click", () => {
+            console.log(scriptContentContainer.value);
+        });
+    
+        var viewButton = document.createElement("button");
+        viewButton.innerText = "View script";
+    
+        var isViewed = false;
+        viewButton.addEventListener("click", () => {
+            if (isViewed == false) {
+                isViewed = true;
+                scriptContentContainer.setAttribute("style", "display: block;");
+            } else {
+                isViewed = false;
+                scriptContentContainer.setAttribute("style", "display: none;");
+            }
+        });
+    
+        var deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+    
+        deleteButton.addEventListener("click", () => {
+            scriptsLibPage.getElementsByClassName("row")[0].removeChild(scriptContainer);
+        });
+    
+        scriptButtons.appendChild(executeButton);
+        scriptButtons.appendChild(viewButton);
+        scriptButtons.appendChild(deleteButton);
+    
+        scriptContainer.appendChild(scriptH3);
+        scriptContainer.appendChild(scriptP);
+        scriptContainer.appendChild(scriptButtons);
+        scriptContainer.appendChild(scriptContentContainer);
+    
+        scriptsLibPage.getElementsByClassName("row")[0].appendChild(scriptContainer);
+    } else {
+        var scriptContainer = document.createElement("div");
+        scriptContainer.setAttribute("class", "script");
+    
+        var scriptH3 = document.createElement("h3");
+        scriptH3.innerText = scriptName.value;
+    
+        var scriptP = document.createElement("p");
+        scriptP.innerText = scriptDesc.value;
+    
+        var scriptButtons = document.createElement("div");
+        scriptButtons.setAttribute("class", "buttons");
+    
+        var scriptContentContainer = document.createElement("div");
+        scriptContentContainer.setAttribute("class", "content");
+        scriptContentContainer.setAttribute("style", "display: none;");
+    
+        var scriptContentP = document.createElement("p");
+        scriptContentP.innerText = "Script";
+    
+        var scriptContentTextArea = document.createElement("textarea");
+        scriptContentTextArea.value = scriptContent.value;
+    
+        scriptContentContainer.appendChild(scriptContentP);
+        scriptContentContainer.appendChild(scriptContentTextArea);
+    
+        var executeButton = document.createElement("button");
+        executeButton.innerText = "Execute";
+    
+        executeButton.addEventListener("click", () => {
+            console.log(scriptContentContainer.value);
+        });
+    
+        var viewButton = document.createElement("button");
+        viewButton.innerText = "View script";
+    
+        var isViewed = false;
+        viewButton.addEventListener("click", () => {
+            if (isViewed == false) {
+                isViewed = true;
+                scriptContentContainer.setAttribute("style", "display: block;");
+            } else {
+                isViewed = false;
+                scriptContentContainer.setAttribute("style", "display: none;");
+            }
+        });
+    
+        var deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+    
+        deleteButton.addEventListener("click", () => {
+            scriptsLibPage.getElementsByClassName("row")[0].removeChild(scriptContainer);
+        });
+    
+        scriptButtons.appendChild(executeButton);
+        scriptButtons.appendChild(viewButton);
+        scriptButtons.appendChild(deleteButton);
+    
+        scriptContainer.appendChild(scriptH3);
+        scriptContainer.appendChild(scriptP);
+        scriptContainer.appendChild(scriptButtons);
+        scriptContainer.appendChild(scriptContentContainer);
+    
+        scriptsLibPage.getElementsByClassName("row")[0].appendChild(scriptContainer);
+    }
 }
 
-addRow_click = () => {
-    var scriptsLibPage = document.getElementsByClassName("scriptsLibPage")[0];
-    var newRow = document.createElement("div");
-    newRow.setAttribute("class", "column");
-    scriptsLibPage.appendChild(newRow);
+addScriptInput_oninput = () => {
+    if (scriptName.value.length >= 5 && scriptContent.value.length >= 5) {
+        scriptsLibPage.getElementsByTagName("button")[0].disabled = false;
+    } else {
+        scriptsLibPage.getElementsByTagName("button")[0].disabled = true;
+    }
 }
