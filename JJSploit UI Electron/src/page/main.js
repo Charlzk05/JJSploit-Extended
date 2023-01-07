d2a0adbc7fbcf1d89ef70ab54e570d3761bba1ab
charlzk05 <charliezkiecharlzk@gmail.com>
2023-01-07 15:36:28 +0800

    a

--- JJSploit UI Electron/src/page/main.js
+++ JJSploit UI Electron/src/page/main.js
@@ -46,3 +46,46 @@ scriptsLib_click = () => {
     buttonsPage.setAttribute("style", "display: none;");
     scriptsLibPage.setAttribute("style", "display: block;");
 }
+
+addScript_click = () => {
+    var scriptName = document.getElementById("scriptName");
+    var scriptDesc = document.getElementById("scriptDesc");
+    var scriptContent = document.getElementById("scriptContent");
+    var scriptsLibPage = document.getElementsByClassName("scriptsLibPage")[0];
+
+    Array.from(scriptsLibPage.getElementsByClassName("column")).forEach((column) => {
+        if (column.getElementsByClassName("script").length >= 3) { } else {
+            var scriptContainer = document.createElement("div");
+            scriptContainer.setAttribute("class", "script");
+            var scriptH3 = document.createElement("h3");
+            scriptH3.innerText = scriptName.value;
+            var scriptP = document.createElement("p");
+            scriptP.innerText = scriptDesc.value;
+            var scriptButtons = document.createElement("div");
+            scriptButtons.setAttribute("class", "buttons");
+            var executeButton = document.createElement("button");
+            executeButton.innerText = "Execute";
+            var deleteButton = document.createElement("button");
+            deleteButton.innerText = "Delete";
+            scriptButtons.appendChild(executeButton);
+            scriptButtons.appendChild(deleteButton);
+            var scriptTextArea = document.createElement("textarea");
+            scriptTextArea.setAttribute("style", "display: none;");
+            scriptTextArea.value = scriptContent.value;
+
+            scriptContainer.appendChild(scriptH3);
+            scriptContainer.appendChild(scriptP);
+            scriptContainer.appendChild(scriptButtons);
+            scriptContainer.appendChild(scriptTextArea);
+
+            column.appendChild(scriptContainer);
+        }
+    });
+}
+
+addRow_click = () => {
+    var scriptsLibPage = document.getElementsByClassName("scriptsLibPage")[0];
+    var newRow = document.createElement("div");
+    newRow.setAttribute("class", "column");
+    scriptsLibPage.appendChild(newRow);
+}
