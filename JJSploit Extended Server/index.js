6f455e1c58db3e5412368c04531393256ceb07ab
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2023-01-21 20:38:53 +0800

    Added JJSploit Server

---
+++ JJSploit Extended Server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1 style='font-weight: 500;'>Hello World</h1>");
+});
+
+app.post("/DownloadConsoleApp", (req, res) => {
+    res.download(path.join(__dirname, "Downloads", "JJSploit Extended Server.zip"));
+});
+
+app.listen(port, () => {
+    console.log(`Express server is listening on port ${port}!`);
+});
