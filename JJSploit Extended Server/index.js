1143f457b9060d0b8496b4469c43b8ff193df1e4
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2023-01-21 20:40:05 +0800

    Change font

--- JJSploit Extended Server/index.js
+++ JJSploit Extended Server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1 style='font-weight: 500;'>Hello World</h1>");
+    res.send("<h1 style='font-weight: 500; font-family: Segoe UI;'>Hello World</h1>");
 });
 
 app.post("/DownloadConsoleApp", (req, res) => {
